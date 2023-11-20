35d691ceef627615ee4d009198098c3da823e694
Riccardi.20248 <Riccardi.20248@LABI2-13PC15>
2023-11-20 09:13:53 +0100

    1 - classe prodotto

--- 1 - classe prodotto/Program.cs
+++ 1 - classe prodotto/Program.cs
@@ -6,11 +6,36 @@ using System.Threading.Tasks;
 
 namespace _1___classe_prodotto
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+        class Prodotto
         {
+            private string nome;
+            private double prezzo;
+            public Prodotto()
+            {
+                nome = "";
+                prezzo = 0;
+            }
+            public Prodotto(string nome, double prezzo)
+            {
+                this.nome = nome;
+                this.prezzo = prezzo;
+            }
 
+            public string ottieninome()
+            {
+                return nome;
+            }
+            public double ottieniprezzo()
+            {
+                return prezzo;
+            }
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            }
         }
     }
-}
+
